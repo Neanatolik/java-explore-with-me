e1bbbb8e765d27ada800a4d885f227be1f00f549
Neanatolik <TimofeevAnVl@yandex.ru>
2024-04-27 19:17:16 +0300

    feat: dp

--- stats/stats-server/src/main/java/ru/practicum/StatsServer.java
+++ stats/stats-server/src/main/java/ru/practicum/StatsServer.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StatsServer
-{
+public class StatsServer {
     public static void main( String[] args )
     {
         SpringApplication.run(StatsServer.class, args);
