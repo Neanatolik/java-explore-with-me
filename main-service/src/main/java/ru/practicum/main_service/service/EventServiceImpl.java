8a262cd29311c874ac3e9af3191f5227b4c0c361
Neanatolik <TimofeevAnVl@yandex.ru>
2024-05-09 02:28:46 +0300

    refactor: dp 2 review 1

--- main-service/src/main/java/ru/practicum/main_service/service/EventServiceImpl.java
+++ main-service/src/main/java/ru/practicum/main_service/service/EventServiceImpl.java
@@ -316,7 +316,7 @@ public class EventServiceImpl implements EventService {
         }
         return event;
     }
-    
+
     private void checkNewEventDto(NewEventDto newEventDto) {
         if (LocalDateTime.parse(newEventDto.getEventDate(), DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")).isBefore(LocalDateTime.now().plusHours(2))) {
             throw new BadRequest("event date раньше чем через 2 часа", "Ошибка запроса");
